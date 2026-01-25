9369ddf24eac9f266971fdbde3f0681c3001afb8
MD SAFI MAAZ <98162729+SafiMaaz01@users.noreply.github.com>
2026-01-25 19:52:41 +0530

    Blog Creation
    1. Blog Creation Route
    2. Creating the Database Schema
    3. Creating First Server Function (Endpoint)
    4. Client-Side Mutation

---
+++ app/(shared-layout)/create/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { postSchema } from "@/app/schemas/blog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useTransition } from "react";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export default function CreateRoute() {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const mutation = useMutation(api.posts.createPost);
+  const form = useForm({
+    resolver: zodResolver(postSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof postSchema>) {
+    startTransition(() => {
+      mutation({ title: values.title, content: values.content });
+
+      toast.success("Post created successfully");
+      router.push("/");
+    });
+  }
+  return (
+    <div className="py-4">
+      <div className="text-center mb-12">
+        <h1 className="text-3xl font-extrabold tracking-tight sm:text-5xl">
+          Create Post
+        </h1>
+        <p className="pt-4 text-xl text-muted-foreground">
+          Share your thoughts and ideas with the world.
+        </p>
+      </div>
+      <Card className="w-full max-w-xl mx-auto">
+        <CardHeader>
+          <CardTitle>Create Blog Article</CardTitle>
+          <CardDescription>
+            Create a new blog article to share with the world.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup className="gap-y-4">
+              <Controller
+                name="title"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel>Title</FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your title"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="content"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel>Content</FieldLabel>
+                    <Textarea
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Enter your content"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Button disabled={isPending}>
+                {isPending ? (
+                  <>
+                    <Loader2 className="size-4 animate-spin" />
+                    <span>Creating Post...</span>
+                  </>
+                ) : (
+                  <span>Create Post</span>
+                )}
+              </Button>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ convex/posts.ts
@@ -0,0 +1,23 @@
+import { mutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { authComponent } from "./auth";
+
+//Create a new task with the given title and description
+export const createPost = mutation({
+    args: {
+        title: v.string(),
+        content: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const user = await authComponent.safeGetAuthUser(ctx);  
+        if (!user) {
+            throw new ConvexError("User not authenticated");
+        }    
+        const blogArticle = await ctx.db.insert("posts", {
+            title: args.title,
+            content: args.content,
+            authorId: user._id,
+        });
+        return blogArticle;
+    },
+});
