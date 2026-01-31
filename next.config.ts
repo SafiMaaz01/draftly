27a20dd7ffba0623ffdaa3d48546b1b09e371594
MD SAFI MAAZ <98162729+SafiMaaz01@users.noreply.github.com>
2026-01-31 21:03:19 +0530

    Fixed Blog Image
    create Blog Image is now availabe in the deployed url.

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "quick-chameleon-170.convex.cloud",
+        hostname: "reminiscent-turtle-32.convex.cloud",
         port: "",
       },
     ],
